29336eaffcba289ba406418f7079b859661e7175
Dave Moten <davidmoten@gmail.com>
2014-06-13 22:56:26 +1000

    format, rename

--- src/main/java/com/github/davidmoten/rx/slf4j/SingleSubscriptionSubject.java
+++ src/main/java/com/github/davidmoten/rx/slf4j/SingleSubscriptionSubject.java
@@ -7,56 +7,57 @@ import rx.subjects.Subject;
 
 public class SingleSubscriptionSubject<T> extends Subject<T, T> {
 
-	private final OnSubscribeImpl<T> onSubscribe;
+    private final OnSubscribeSingleOnly<T> onSubscribe;
 
-	public static <T> SingleSubscriptionSubject<T> create() {
-		OnSubscribeImpl<T> onSubscribe = new OnSubscribeImpl<T>();
-		SingleSubscriptionSubject<T> subject = new SingleSubscriptionSubject<T>(
-				onSubscribe);
-		return subject;
-	}
+    public static <T> SingleSubscriptionSubject<T> create() {
+        OnSubscribeSingleOnly<T> onSubscribe = new OnSubscribeSingleOnly<T>();
+        SingleSubscriptionSubject<T> subject = new SingleSubscriptionSubject<T>(onSubscribe);
+        return subject;
+    }
 
-	public SingleSubscriptionSubject(OnSubscribeImpl<T> onSubscribe) {
-		super(onSubscribe);
-		this.onSubscribe = onSubscribe;
-	}
+    public SingleSubscriptionSubject(OnSubscribeSingleOnly<T> onSubscribe) {
+        super(onSubscribe);
+        this.onSubscribe = onSubscribe;
+    }
 
-	@Override
-	public void onCompleted() {
-		if (subscribed())
-			onSubscribe.getSubscriber().get().onCompleted();
-	}
+    @Override
+    public void onCompleted() {
+        if (subscribed())
+            onSubscribe.getSubscriber().get().onCompleted();
+    }
 
-	@Override
-	public void onError(Throwable e) {
-		if (subscribed())
-			onSubscribe.getSubscriber().get().onError(e);
-	}
+    @Override
+    public void onError(Throwable e) {
+        if (subscribed())
+            onSubscribe.getSubscriber().get().onError(e);
+    }
 
-	@Override
-	public void onNext(T t) {
-		if (subscribed())
-			onSubscribe.getSubscriber().get().onNext(t);
-	}
+    @Override
+    public void onNext(T t) {
+        if (subscribed())
+            onSubscribe.getSubscriber().get().onNext(t);
+    }
 
-	private boolean subscribed() {
-		return onSubscribe.getSubscriber().get() != null
-				&& onSubscribe.getSubscriber().get().isUnsubscribed();
-	}
+    private boolean subscribed() {
+        return onSubscribe.getSubscriber().get() != null && onSubscribe.getSubscriber().get().isUnsubscribed();
+    }
 
-	private static class OnSubscribeImpl<T> implements OnSubscribe<T> {
+    private static class OnSubscribeSingleOnly<T> implements OnSubscribe<T> {
 
-		private final AtomicReference<Subscriber<? super T>> sub = new AtomicReference<Subscriber<? super T>>();
+        private final AtomicReference<Subscriber<? super T>> sub = new AtomicReference<Subscriber<? super T>>();
 
-		@Override
-		public void call(Subscriber<? super T> subscriber) {
-			sub.set(subscriber);
-		}
+        @Override
+        public void call(Subscriber<? super T> subscriber) {
+            if (sub.get() == null)
+                subscriber.onError(new RuntimeException("Only one subscription allowed"));
+            else
+                sub.set(subscriber);
+        }
 
-		public AtomicReference<Subscriber<? super T>> getSubscriber() {
-			return sub;
-		}
+        public AtomicReference<Subscriber<? super T>> getSubscriber() {
+            return sub;
+        }
 
-	}
+    }
 
 }
--- src/test/java/com/github/davidmoten/rx/slf4j/LoggingTest.java
+++ src/test/java/com/github/davidmoten/rx/slf4j/LoggingTest.java
@@ -12,111 +12,105 @@ import com.github.davidmoten.rx.slf4j.Logging.Level;
 
 public class LoggingTest {
 
-	@Test
-	public void testName() {
-		int count = Observable.range(1, 10)
-		// log all
-				.lift(logger(LoggingTest.class).showValue().log())
-				// count
-				.count().toBlocking().single();
-		assertEquals(10, count);
-	}
+    @Test
+    public void testName() {
+        int count = Observable.range(1, 10)
+        // log all
+                .lift(logger(LoggingTest.class).showValue().log())
+                // count
+                .count().toBlocking().single();
+        assertEquals(10, count);
+    }
 
-	@Test
-	public void testCountEvery() {
-		int count = Observable
-				.range(1, 10)
-				// log all
-				.lift(logger().name("rx.Server").prefix("count every test")
-						.excludeValue().subscribed(Level.DEBUG)
-						.onCompleted(Level.DEBUG).showCount("files").every(2)
-						.log())
-				// count
-				.count().toBlocking().single();
-		assertEquals(10, count);
-	}
+    @Test
+    public void testCountEvery() {
+        int count = Observable
+                .range(1, 10)
+                // log all
+                .lift(logger().name("rx.Server").prefix("count every test").excludeValue().subscribed(Level.DEBUG)
+                        .onCompleted(Level.DEBUG).showCount("files").every(2).log())
+                // count
+                .count().toBlocking().single();
+        assertEquals(10, count);
+    }
 
-	@Test
-	public void testAgain() {
-		int count = Observable
-				.range(51, 10)
-				// log all
-				.lift(logger().showCount().start(2).finish(2).showStackTrace()
-						.showMemory().log())
-				// count
-				.count().toBlocking().single();
-		assertEquals(10, count);
-	}
+    @Test
+    public void testAgain() {
+        int count = Observable.range(51, 10)
+        // log all
+                .lift(logger().showCount().start(2).finish(2).showStackTrace().showMemory().log())
+                // count
+                .count().toBlocking().single();
+        assertEquals(10, count);
+    }
 
-	@Test
-	public void testSubscribe() {
-		int count = Observable
-				.range(11, 3010)
-				.lift(logger().showValue().showCount().every(1000).showMemory()
-						.log()).count().toBlocking().single();
-		assertEquals(3010, count);
+    @Test
+    public void testSubscribe() {
+        int count = Observable.range(11, 3010).lift(logger().showValue().showCount().every(1000).showMemory().log())
+                .count().toBlocking().single();
+        assertEquals(3010, count);
 
-	}
+    }
 
-	@Test
-	public void testCallingClass() {
+    @Test
+    public void testCallingClass() {
 
-		assertEquals(10, new CallingClass().count());
-	}
+        assertEquals(10, new CallingClass().count());
+    }
 
-	private static class CallingClass {
-		public int count() {
-			return Observable.range(1, 10)
-			// log all
-					.lift(logger().showValue().log())
-					// count
-					.count().toBlocking().single();
-		}
-	}
+    private static class CallingClass {
+        public int count() {
+            return Observable.range(1, 10)
+            // log all
+                    .lift(logger().showValue().log())
+                    // count
+                    .count().toBlocking().single();
+        }
+    }
 
-	@Test
-	public void testKitchenSink() {
-		Observable.range(1, 100)
-		// log
-				.lift(Logging.<Integer> logger("Boo")
-				// count
-						.showCount()
-						// start on 2nd item
-						.start(2)
-						// ignore after 8th item
-						.finish(18)
-						// take every third item
-						.every(3)
-						// set the onCompleted message
-						.onCompleted("finished")
-						// at logging level
-						.onCompleted(Level.INFO)
-						// set the error logging level
-						.onError(Level.WARN)
-						// onNext at debug level
-						.onNext(Level.DEBUG)
-						// how to format the onNext item
-						.onNextFormat("time=%sdays")
-						// show subscribed message at INFO level
-						.subscribed(Level.INFO)
-						// the message to show at subscription time
-						.subscribed("created subscription")
-						// the unsubscribe message at DEBUG level
-						.unsubscribed(Level.DEBUG)
-						// the unsubscribe message
-						.unsubscribed("ended subscription")
-						// only when item is an even number
-						.when(new Func1<Integer, Boolean>() {
-							@Override
-							public Boolean call(Integer n) {
-								return n % 2 == 0;
-							}
-						})
-						// count those items passing the filters above
-						.showCount("finalCount")
-						// build the operator
-						.log())
-				// block and get the answer
-				.toBlocking().last();
-	}
+    @Test
+    public void testKitchenSink() {
+        Observable.range(1, 100)
+        // log
+                .lift(Logging.<Integer> logger("Boo")
+                // count
+                        .showCount()
+                        // start on 2nd item
+                        .start(2)
+                        // ignore after 8th item
+                        .finish(18)
+                        // take every third item
+                        .every(3)
+                        // set the onCompleted message
+                        .onCompleted("finished")
+                        // at logging level
+                        .onCompleted(Level.INFO)
+                        // set the error logging level
+                        .onError(Level.WARN)
+                        // onNext at debug level
+                        .onNext(Level.DEBUG)
+                        // how to format the onNext item
+                        .onNextFormat("time=%sdays")
+                        // show subscribed message at INFO level
+                        .subscribed(Level.INFO)
+                        // the message to show at subscription time
+                        .subscribed("created subscription")
+                        // the unsubscribe message at DEBUG level
+                        .unsubscribed(Level.DEBUG)
+                        // the unsubscribe message
+                        .unsubscribed("ended subscription")
+                        // only when item is an even number
+                        .when(new Func1<Integer, Boolean>() {
+                            @Override
+                            public Boolean call(Integer n) {
+                                return n % 2 == 0;
+                            }
+                        })
+                        // count those items passing the filters above
+                        .showCount("finalCount")
+                        // build the operator
+                        .log())
+                // block and get the answer
+                .toBlocking().last();
+    }
 }
