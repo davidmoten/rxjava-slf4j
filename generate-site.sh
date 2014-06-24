1810df90b51a509926deb587f3f300c802493cda
Dave Moten <davidmoten@gmail.com>
2014-06-24 21:21:03 +1000

    fix site

--- generate-site.sh
+++ generate-site.sh
@@ -3,7 +3,7 @@ set -e
 mvn site
 cd ../davidmoten.github.io
 git pull
-mkdir rxjava-slf4j
+mkdir -p rxjava-slf4j
 cp -r ../rxjava-slf4j/target/site/* rxjava-slf4j/
 git add .
 git commit -am "update site reports"
