533fab71d09d5d5307d6b18ff66dfadd5fba7ab4
Dave Moten <davidmoten@gmail.com>
2015-06-21 19:39:51 +1000

    if logging transformations throw and exception then log an error

--- src/main/java/com/github/davidmoten/rx/slf4j/OperatorLogging.java
+++ src/main/java/com/github/davidmoten/rx/slf4j/OperatorLogging.java
@@ -1,13 +1,15 @@
 package com.github.davidmoten.rx.slf4j;
 
 import static com.github.davidmoten.rx.slf4j.Logging.log;
+
+import org.slf4j.Logger;
+
 import rx.Notification;
 import rx.Observable;
 import rx.Observable.Operator;
 import rx.Subscriber;
 import rx.functions.Action0;
 import rx.functions.Func1;
-import rx.observers.Subscribers;
 
 import com.github.davidmoten.rx.slf4j.Logging.Parameters;
 import com.github.davidmoten.rx.slf4j.Logging.Parameters.Message;
@@ -15,106 +17,135 @@ import com.github.davidmoten.rx.subjects.PublishSubjectSingleSubscriber;
 
 public class OperatorLogging<T> implements Operator<T, T> {
 
-    private final Parameters<T> parameters;
+	private final Parameters<T> parameters;
 
-    public OperatorLogging(Parameters<T> parameters) {
-        this.parameters = parameters;
-    }
+	public OperatorLogging(Parameters<T> parameters) {
+		this.parameters = parameters;
+	}
 
-    @Override
-    public Subscriber<? super T> call(Subscriber<? super T> child) {
-        // create the subject and an observable from the subject that
-        // materializes the notifications from the subject
-        PublishSubjectSingleSubscriber<T> subject = PublishSubjectSingleSubscriber.create();
+	@Override
+	public Subscriber<? super T> call(Subscriber<? super T> child) {
+		// create the subject and an observable from the subject that
+		// materializes the notifications from the subject
+		PublishSubjectSingleSubscriber<T> subject = PublishSubjectSingleSubscriber
+				.create();
 
-        // create the logging observable
-        Observable<Message<T>> observable = createObservableFromSubject(subject);
+		// create the logging observable
+		Observable<Message<T>> observable = createObservableFromSubject(subject);
 
-        // apply all the logging stream transformations
-        for (Func1<Observable<Message<T>>, Observable<Message<T>>> transformation : parameters
-                .getTransformations()) {
-            observable = transformation.call(observable);
-        }
+		// apply all the logging stream transformations
+		for (Func1<Observable<Message<T>>, Observable<Message<T>>> transformation : parameters
+				.getTransformations()) {
+			observable = transformation.call(observable);
+		}
 
-        Action0 unsubscriptionLogger = createUnsubscriptionAction(parameters);
-        Action0 subscriptionLogger = createSubscriptionAction(parameters);
-        observable = observable
-        // add subscription action
-                .doOnSubscribe(subscriptionLogger)
-                // add unsubscription action
-                .doOnUnsubscribe(unsubscriptionLogger);
+		Action0 unsubscriptionLogger = createUnsubscriptionAction(parameters);
+		Action0 subscriptionLogger = createSubscriptionAction(parameters);
+		observable = observable
+		// add subscription action
+				.doOnSubscribe(subscriptionLogger)
+				// add unsubscription action
+				.doOnUnsubscribe(unsubscriptionLogger);
 
-        // create parent subscriber
-        Subscriber<T> parent = createSubscriber(subject, child);
+		// create parent subscriber
+		Subscriber<T> parent = createParentSubscriber(subject, child);
 
-        // create subscriber for the logging observable
-        Subscriber<Message<T>> logSubscriber = Subscribers.empty();
+		// create subscriber for the logging observable
+		Subscriber<Message<T>> logSubscriber = createErrorLoggingSubscriber(parameters
+				.getLogger());
 
-        // ensure logSubscriber is unsubscribed when child is unsubscribed
-        child.add(logSubscriber);
+		// ensure logSubscriber is unsubscribed when child is unsubscribed
+		child.add(logSubscriber);
 
-        // subscribe to the logging observable
-        observable.unsafeSubscribe(logSubscriber);
+		// subscribe to the logging observable
+		observable.unsafeSubscribe(logSubscriber);
 
-        return parent;
-    }
+		return parent;
+	}
 
-    private static <T> Observable<Message<T>> createObservableFromSubject(
-            PublishSubjectSingleSubscriber<T> subject) {
-        return subject.materialize().map(new Func1<Notification<T>, Message<T>>() {
+	private static <T> Subscriber<Message<T>> createErrorLoggingSubscriber(
+			final Logger logger) {
+		return new Subscriber<Message<T>>() {
 
-            @Override
-            public Message<T> call(Notification<T> n) {
-                return new Message<T>(n, "");
-            }
-        });
-    }
+			@Override
+			public void onCompleted() {
+				// ignore
+			}
 
-    private static <T> Action0 createUnsubscriptionAction(final Parameters<T> p) {
-        return new Action0() {
-            @Override
-            public void call() {
-                // log unsubscription if requested
-                if (p.getUnsubscribedMessage() != null)
-                    log(p.getLogger(), p.getUnsubscribedMessage(), p.getUnsubscribedLevel(), null);
-            }
+			@Override
+			public void onError(Throwable e) {
+				logger.error(
+						"the logging transformations through an exception: "
+								+ e.getMessage(), e);
+			}
 
-        };
-    }
+			@Override
+			public void onNext(Message<T> t) {
+				// ignore
+			}
+		};
+	}
 
-    private static <T> Action0 createSubscriptionAction(final Parameters<T> p) {
-        return new Action0() {
-            @Override
-            public void call() {
-                // log subscription if requested
-                if (p.getSubscribedMessage() != null)
-                    log(p.getLogger(), p.getSubscribedMessage(), p.getSubscribedLevel(), null);
-            }
-        };
-    }
+	private static <T> Observable<Message<T>> createObservableFromSubject(
+			PublishSubjectSingleSubscriber<T> subject) {
+		return subject.materialize().map(
+				new Func1<Notification<T>, Message<T>>() {
 
-    private static <T> Subscriber<T> createSubscriber(
-            final PublishSubjectSingleSubscriber<T> subject, final Subscriber<? super T> child) {
-        return new Subscriber<T>(child) {
+					@Override
+					public Message<T> call(Notification<T> n) {
+						return new Message<T>(n, "");
+					}
+				});
+	}
 
-            @Override
-            public void onCompleted() {
-                subject.onCompleted();
-                child.onCompleted();
-            }
+	private static <T> Action0 createUnsubscriptionAction(final Parameters<T> p) {
+		return new Action0() {
+			@Override
+			public void call() {
+				// log unsubscription if requested
+				if (p.getUnsubscribedMessage() != null)
+					log(p.getLogger(), p.getUnsubscribedMessage(),
+							p.getUnsubscribedLevel(), null);
+			}
 
-            @Override
-            public void onError(Throwable e) {
-                subject.onError(e);
-                child.onError(e);
-            }
+		};
+	}
 
-            @Override
-            public void onNext(T t) {
-                subject.onNext(t);
-                child.onNext(t);
-            }
-        };
-    }
+	private static <T> Action0 createSubscriptionAction(final Parameters<T> p) {
+		return new Action0() {
+			@Override
+			public void call() {
+				// log subscription if requested
+				if (p.getSubscribedMessage() != null)
+					log(p.getLogger(), p.getSubscribedMessage(),
+							p.getSubscribedLevel(), null);
+			}
+		};
+	}
+
+	private static <T> Subscriber<T> createParentSubscriber(
+			final PublishSubjectSingleSubscriber<T> subject,
+			final Subscriber<? super T> child) {
+		return new Subscriber<T>(child) {
+
+			@Override
+			public void onCompleted() {
+				subject.onCompleted();
+				child.onCompleted();
+			}
+
+			@Override
+			public void onError(Throwable e) {
+				subject.onError(e);
+				child.onError(e);
+			}
+
+			@Override
+			public void onNext(T t) {
+				subject.onNext(t);
+				child.onNext(t);
+			}
+		};
+	}
 
 }
